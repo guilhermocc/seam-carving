d57f99eda1fa5771d2a8c446f6320699c6df198b
Guilherme Carvalho <guilherme.carvalho@creditas.com.br>
2020-09-03 19:26:23 -0300

    Add solution code for stage 2

--- src/main/kotlin/com.jetacademy.seamcarving/Main.kt
+++ src/main/kotlin/com.jetacademy.seamcarving/Main.kt
@@ -3,27 +3,24 @@ package com.jetacademy.seamcarving
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
-import java.util.*
 import javax.imageio.ImageIO
 
-fun main() {
-    val scanner = Scanner(System.`in`)
-    print("Enter rectangle width:")
-    val width = scanner.nextInt()
-    print("Enter rectangle height:")
-    val height = scanner.nextInt()
-    print("Enter output image name:")
-    val outputFileName = scanner.next()
-    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    val graphics2d = bufferedImage.createGraphics()
-    graphics2d.apply {
-        background = Color.BLACK
-        color = Color.RED
-        drawLine(0, 0, width - 1, height - 1)
-        drawLine(0, height - 1, width - 1, 0)
-    }.also {
-        ImageIO.write(bufferedImage, "png", File(outputFileName))
-    }
-    println("image created")
-
+fun main(args: Array<String>) {
+    val inputFile = args[args.indexOf("-in") + 1]
+    val outputFile = args[args.indexOf("-out") + 1]
+    val bufferedInputImage: BufferedImage = ImageIO.read(File(inputFile))
+    ImageIO.write(bufferedInputImage.invertImageColors(), "png", File(outputFile))
 }
+
+fun BufferedImage.invertImageColors(): BufferedImage =
+    this.apply {
+        repeat(this.width) { x ->
+            repeat(this.height) { y ->
+                val currentColor = Color(this.getRGB(x, y))
+                this.setRGB(
+                    x, y, Color(255 - currentColor.red, 255 - currentColor.green, 255 - currentColor.blue).rgb
+                )
+            }
+        }
+    }
+
